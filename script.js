6b2c569a9b603699ccf7db76739dc44a599ae191
Morgan MDanat <mdanatmorgan3@gmail.com>
2023-04-21 15:42:47 -0600

    style.css changes, Photos, content in body of webpage

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content was added by JavaScript";
-document.querySelector("body").appendChild(h2)
+const h3 = document.createElement("h2");
+h3.textContent = "Contact Info";
+document.querySelector("body").appendChild(h3)
