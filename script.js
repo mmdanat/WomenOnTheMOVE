1579b40a01c32cde4d315f7fd5fe551054581e91
Morgan MDanat <mdanatmorgan3@gmail.com>
2023-04-23 11:22:27 -0600

    Completed Website with JS,CSS & HTML

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h3 = document.createElement("h3");
-h3.textContent = "Contact Info";
+h3.textContent = "Contact Info : mdanatmorgan3@gmail.com";
 document.querySelector("body").appendChild(h3)
 
  const input = document.getElementById('button');
