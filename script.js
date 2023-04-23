bb8ce6b60e1f068fb0f796fd92f4f23232b2a0b5
Morgan MDanat <mdanatmorgan3@gmail.com>
2023-04-23 10:44:35 -0600

    contact info and table, paragraph info

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const h3 = document.createElement("h2");
+const h3 = document.createElement("h3");
 h3.textContent = "Contact Info";
 document.querySelector("body").appendChild(h3)
+
+ const input = document.getElementById('button');
+input.addEventListener('click',function(){
+    alert("Thanks for reaching out!");
+ });
